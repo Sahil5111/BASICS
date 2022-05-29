516430938e5f4a4116b80558fa368fb05d2dea06
Sahil <sahilsingh7977@gmail.com>
2022-05-29 15:05:07 +0530

    added ways to initalise string

--- strings_basic.py
+++ strings_basic.py
@@ -3,4 +3,10 @@ string2=' world!'
 # string concatenation ->str1 +str2
 print(string1+string2)
 
-# getting nth element from a string
+# getting nth element from a string->str[0]for first element and so on
+print(string1[0])
+# ways to initalise a string
+str="string's"
+str2='''I say "python is simple"
+new line can be added this way as well'''
+print(str2) 
