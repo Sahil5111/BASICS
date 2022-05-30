cce4060727c6dd6f15c7c9f32462593753458c27
Sahil <sahilsingh7977@gmail.com>
2022-05-30 19:46:16 +0530

    added slicing of string

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,3 @@
-string1='hello'
-string2=' world!'
-# string concatenation ->str1 +str2
-print(string1+string2)
-
-# getting nth element from a string->str[0]for first element and so on
-print(string1[0])
-# ways to initalise a string
 str="string's"
 str2='''I say "python is simple"
 new line can be added this way as well'''
@@ -16,5 +8,5 @@ print(str[1:7])#slice is between 1 to 6
 # we can access last char in python using negative numbers
 print(str[-1])
 # strings are immutable in python slice is used to change a string
-str=str[0:6]+" " #updates string str to string
+str=str[0:6]+" "
 print(str)
