b3d512a77204e50688c5f7d386d2808f8ef5e4d4
Sahil <sahilsingh7977@gmail.com>
2022-05-31 22:12:27 +0530

    initial commit for control_flow.py

---
+++ control_flow.py
@@ -0,0 +1,7 @@
+# if else statements in python
+a=1
+if a==0 :
+    print("programming is easy")
+print("programming is not easy")
+# only indented part falls in the conditional
+# in python empty sequence is treated as false and any value other than 0 is true 
