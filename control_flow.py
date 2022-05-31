a1d242eff06d99275f5613e56e9007668a4509c3
skp89 <105152639+skp89@users.noreply.github.com>
2022-05-31 22:45:45 +0530

    Update control_flow.py
    else condition

--- control_flow.py
+++ control_flow.py
@@ -2,6 +2,7 @@
 a=1
 if a==0 :
     print("programming is easy")
-print("programming is not easy")
+else :
+    print("programming is not easy")
 # only indented part falls in the conditional
 # in python empty sequence is treated as false and any value other than 0 is true 
