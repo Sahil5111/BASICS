d3dfe727e18b1f52ce9e0e205957d1c696e66008
Sahil <sahilsingh7977@gmail.com>
2022-06-01 21:55:48 +0530

    added for loop in control_flow.py

--- 4_control_flow.py
+++ 4_control_flow.py
@@ -4,8 +4,7 @@ if a==0 :
     print("programming is easy")
 else :
     print("programming is not easy")
-# only indented part falls in the conditional
-# in python empty sequence is treated as false and any value other than 0 is true 
+# only indented part falls in the conditional, in python empty sequence is treated as false and any value other than 0 is true 
 # if else if ladder in python
 if a>10:
     print("number is greater than 10")
@@ -13,3 +12,7 @@ elif 5>a>10:
     print("number is between 5 and 10")
 else :
     print("number is smaller than 5")
+# loops in python 
+for i in range(0,6):#as expected this range ends one before upper limit 
+    print(i)
+# and loop runs for every integral value between limits of range
