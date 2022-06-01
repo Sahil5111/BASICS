ae9c5a76551b00c3ea6c8407a6135211dfbd89dd
Sahil <sahilsingh7977@gmail.com>
2022-06-01 21:47:42 +0530

    added if else ladder in control_flow.py

--- 4_control_flow.py
+++ 4_control_flow.py
@@ -1,8 +1,15 @@
 # if else statements in python
-a=1
+a=11
 if a==0 :
     print("programming is easy")
 else :
     print("programming is not easy")
 # only indented part falls in the conditional
 # in python empty sequence is treated as false and any value other than 0 is true 
+# if else if ladder in python
+if a>10:
+    print("number is greater than 10")
+elif 5>a>10:
+    print("number is between 5 and 10")
+else :
+    print("number is smaller than 5")
