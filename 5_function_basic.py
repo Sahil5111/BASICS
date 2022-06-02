f89634b2950ae527ad997334118c7761b177638c
Sahil <sahilsingh7977@gmail.com>
2022-06-02 21:12:16 +0530

    added function to find list of prime no upto a given number in 5_function_basic.py

--- 5_function_basic.py
+++ 5_function_basic.py
@@ -1,4 +1,8 @@
 # function defination in python 
+from asyncio.windows_events import NULL
+from ctypes import sizeof
+
+
 def sum (a,b):
     return (a+b)
 # indented part is inside the function
@@ -14,3 +18,26 @@ def fact(a):
         return(a*fact(a-1))
 
 print(fact(4))
+
+# prime upto n
+
+
+def isprime(num):
+    li=[]
+    for i in range (1,num+1):
+        if num%i ==0:
+            li=li+[i]
+    if len(li)==2:
+        return True
+    else:
+        return False
+
+
+def primeupto(num):
+    li=[]
+    for i in range (1,num+1):
+        if isprime(i):
+            li=li + [i]
+    return li
+
+print(primeupto(10))
