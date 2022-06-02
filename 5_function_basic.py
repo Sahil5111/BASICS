e86a14aba4743a9e1b50f34998f258c0f554248f
Sahil <sahilsingh7977@gmail.com>
2022-06-02 20:10:23 +0530

    added recursive function 5_function_basic.py

--- 5_function_basic.py
+++ 5_function_basic.py
@@ -1,5 +1,16 @@
 # function defination in python 
 def sum (a,b):
     return (a+b)
-
+# indented part is inside the function
+# function stops as soon as it encounters return statement
 print(sum(5,6))
+# function definition is important before using function
+# recurssion
+# recurssion is calling a function when running the same function
+def fact(a):
+    if a<=0:
+        return (1)
+    else:
+        return(a*fact(a-1))
+
+print(fact(4))
