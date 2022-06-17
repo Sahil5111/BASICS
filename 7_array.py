20dfcecfcf0e301cf812316cc63e72ed2de4763c
Sahil <sahilsingh7977@gmail.com>
2022-06-17 18:54:41 +0530

    added recursive binary search

--- 7_array.py
+++ 7_array.py
@@ -1,5 +1,5 @@
 arr=[1,2,3,4,5,6,7,8,9,10]
-
+# iterative binary search that returns position of the required element
 def binarysearch(a,b):
     low=0
     high=len(a)-1
@@ -14,4 +14,19 @@ def binarysearch(a,b):
             high =mid-1
     return -1
 
+# recursive binary search that returns true if element is in the list and returns false if element is not int the list
+def binarysearch2(a,b):
+    mid=(len(a)-1)//2
+    if len(a)==0:
+        return False
+    if a[mid]==b:
+        return True
+    elif a[mid]>b:
+        return(binarysearch2(a[:mid],b))
+    else :
+        return(binarysearch2(a[mid+1:],b))
+
+
 print(binarysearch(arr,8))
+
+print(binarysearch2(arr,11))
