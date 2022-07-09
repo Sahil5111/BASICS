fef38da85c4e995af14005ef6bdf280d594c99ab
Sahil <sahilsingh7977@gmail.com>
2022-07-09 16:23:53 +0530

    added object node2 to inheritance.py

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -1,24 +1,30 @@
-class father:
+class node1:
     age = 40
     def __init__(self):
-        print("this is init by father")
+        print("this is init by node1")
 
     def work(self):
-        print("working")
+        print("working1")
 
+class node2:
+    age=38
+    def __init__(self) -> None:
+        print("this is init by node2")
+    def work(self):
+        print("working2")
 
-class son (father):
+class son (node1,node2):
     age = 18
 
     def __init__(self):
-        print("this is init by son")
-
+        print("this is init by child")
     def play(self):
         print("playing")
 
 son1 = son()
-dad1=father()
+dad1=node1()
 
 dad1.work()
 son1.play()
-son1.work()
+son1.work()#work funtions calls work from line 6 because when inheriting properties precedence is from left to right
+# i.e interpreter will first check node1for the funtion then check node2
