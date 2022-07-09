dc9375169b3fc79c7eb2c4bd1d842cbfe404a382
Sahil <sahilsingh7977@gmail.com>
2022-07-09 16:10:03 +0530

    added basic logic of inheritance to inheritance.py

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -1,11 +1,24 @@
 class father:
-  age=40
-  def __init__(self):
-    print("this is init by father")
+    age = 40
+    def __init__(self):
+        print("this is init by father")
+
+    def work(self):
+        print("working")
+
 
 class son (father):
-    age=18
-    def __init__(self):
-       print("this is init by son")
+    age = 18
 
-son1=son()
+    def __init__(self):
+        print("this is init by son")
+
+    def play(self):
+        print("playing")
+
+son1 = son()
+dad1=father()
+
+dad1.work()
+son1.play()
+son1.work()
