b261e6ee03eb06d653fd86b203f18f3a482988f8
Sahil <sahilsingh7977@gmail.com>
2022-07-31 15:53:02 +0530

    added program to take user input

---
+++ 8_user_input.py
@@ -0,0 +1,6 @@
+a = input("enter your name :")
+print("your name is "+a)#here a is string as input function takes a string as an input
+# to take an integer input 
+b=int(input("enter an integer"))
+print(b)
+#this program will expect an integer input and any other type of input will throw an error
